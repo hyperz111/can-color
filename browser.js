c3ec0b74661d0c673cee862b26f7ff3b494853cd
hyperz111 <firzeroun11@gmail.com>
2026-01-18 22:43:42 +0700

    fix: fix detection on browser.js

--- browser.js
+++ browser.js
@@ -2,5 +2,5 @@
 /* eslint-disable n/no-unsupported-features/node-builtins */
 
 export function canColor() {
-	return ('navigator' in globalThis) && /\b(Chrome|Chromium)\//.test(globalThis.navigator.userAgent);
+	return ('navigator' in globalThis) && ((globalThis.navigator.userAgentData && navigator.userAgentData.brands.find(({brand}) => brand === 'Chromium')?.version > 93) || /\b(Chrome|Chromium)\//.test(globalThis.navigator.userAgent));
 }
