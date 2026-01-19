db043f4491c2173960a8590bd6e670267e1d04fd
hyperz111 <firzeroun11@gmail.com>
2026-01-19 14:55:25 +0700

    refactor: simplify browser.js

--- browser.js
+++ browser.js
@@ -4,8 +4,7 @@
 export function canColor() {
 	return (
 		"navigator" in globalThis &&
-		((globalThis.navigator.userAgentData &&
-			navigator.userAgentData.brands.find(({ brand }) => brand === "Chromium")?.version > 93) ||
+		(globalThis.navigator.userAgentData?.brands?.find(({ brand }) => brand === "Chromium")?.version > 93 ||
 			/\b(Chrome|Chromium)\//.test(globalThis.navigator.userAgent))
 	);
 }
